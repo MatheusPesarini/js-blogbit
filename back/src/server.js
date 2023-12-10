dae97e5576658ed9c2db6c0680dbc9d5beeb6ce5
MatheusPato <mrogeriopesarini@gmail.com>
2023-12-10 04:00:05 -0300

    Modificações do backend

--- back/src/server.js
+++ back/src/server.js
@@ -18,6 +18,13 @@ const config = { // Configuração de acesso ao banco de dados
 // Cria um cliente PostgreSQL
 const client = new Client(config);
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-origin", "*")
+    res.setHeader('Access-Control-Allow-Methods', "GET,POST,OPTIONS")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next();
+})
+
 // Conecta ao banco de dados PostgreSQL
 client.connect()
     .then(() => console.log('Conexão bem-sucedida com o PostgreSQL'))
@@ -31,6 +38,11 @@ app.get('/posts', async(req, res) => {
     const posts = await client.query('SELECT * FROM posts'); // Consultando a tabela posts do banco de dados
     res.send(posts.rows);
 }); 
+
+app.post("/upload-post", (req, res) => {
+    console.log("informacoes post estao sendo enviadas", req.body);
+    res.sendStatus(200);
+});
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta http://localhost:${port}`); // configurando o link com a porta do servidor
